50df1bbc9432efadb4c2979d7747dd6c14eb7dda
lukorito <kelvin@stitch.money>
2023-07-14 21:55:26 +0300

    complete modals

--- lib/home/sections/get_test_tokens.dart
+++ lib/home/sections/get_test_tokens.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:responsive_framework/responsive_breakpoints.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:responsive_framework/responsive_row_column.dart';
 
 import '../../shared/theme.dart';
 import '../../shared/utils/theme_color.dart';
@@ -26,9 +29,175 @@ class _GetTestTokensState extends State<GetTestTokens> {
   String? selectedNetwork = 'Valhalla';
 
   bool isCDropDownOpen = false;
+  String transactionHash = 'a1075db55d416d3ca199f55b6e2115b9345e16c5cf302fc80e9d5fb';
 
   /// validate is used to validate the input field
   bool validate = false;
+
+  Future<void> _errorDialogBuilder(BuildContext context) {
+    final isMobile = ResponsiveBreakpoints.of(context).equals(MOBILE);
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: ResponsiveRowColumn(
+            layout: isMobile ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+            rowMainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ResponsiveRowColumnItem(
+                child: Text(
+                  'Something went wrong...',
+                  style: titleLarge(context),
+                ),
+              ),
+              ResponsiveRowColumnItem(
+                child: IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ],
+          ),
+          content: SizedBox(
+            width: 400.0,
+            child: Text(
+              'Your request failed. Please try again later.',
+              style: bodyMedium(context),
+            ),
+          ),
+          actionsPadding: const EdgeInsets.all(16),
+        );
+      },
+    );
+  }
+
+  Future<void> _successDialogBuilder(BuildContext context) {
+    final isMobile = ResponsiveBreakpoints.of(context).equals(MOBILE);
+
+    final isTablet = ResponsiveBreakpoints.of(context).equals(TABLET);
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: ResponsiveRowColumn(
+            layout: isMobile ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+            rowMainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ResponsiveRowColumnItem(
+                child: Text(
+                  'Confirmed!',
+                  style: titleLarge(context),
+                ),
+              ),
+              ResponsiveRowColumnItem(
+                child: IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ],
+          ),
+          content: SizedBox(
+            width: 550.0,
+            height: 300.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Your request was successful.',
+                  style: bodyMedium(context),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  'Your txn hash',
+                  style: titleSmall(context),
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                  width: double.infinity,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color.fromRGBO(112, 64, 236, 0.04),
+                  ),
+                  child: ResponsiveRowColumn(
+                    layout: isTablet ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+                    children: [
+                      ResponsiveRowColumnItem(
+                        child: SelectableText(
+                          transactionHash,
+                          style: bodyMedium(context),
+                        ),
+                      ),
+                      const ResponsiveRowColumnItem(
+                        child: SizedBox(width: 8),
+                      ),
+                      ResponsiveRowColumnItem(
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.copy,
+                            color: Color(0xFF858E8E),
+                          ),
+                          onPressed: () {
+                            Clipboard.setData(
+                              ClipboardData(text: transactionHash),
+                            );
+                            toast.showToast(
+                              child: const Text('Copied to clipboard'),
+                              gravity: ToastGravity.BOTTOM,
+                              toastDuration: const Duration(seconds: 2),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      //  TODO: Add link to explorer using url_launcher
+                    },
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF0DC8D4),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'View in Annulus Topl Explorer',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: "Rational Display",
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          actionsPadding: const EdgeInsets.all(4),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -191,12 +360,30 @@ class _GetTestTokensState extends State<GetTestTokens> {
               height: 24,
             ),
             TextField(
+              // TODO: Add to accept the address format only
               controller: textWalletEditingController,
               style: bodyMedium(context),
               decoration: InputDecoration(
                 labelText: 'Wallet Address',
                 labelStyle: bodyMedium(context),
                 hintText: '0xxxxxxxxxxxxxxxxxxxxxxxxx',
+                suffix: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: const Color(0xFFC0C4C4),
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  onPressed: () async {
+                    final copiedData = await Clipboard.getData('text/plain');
+                    textWalletEditingController.value = TextEditingValue(
+                      text: copiedData?.text ?? '',
+                      selection: TextSelection.fromPosition(
+                        TextPosition(offset: copiedData?.text?.length ?? 0),
+                      ),
+                    );
+                  },
+                  child: const Text('Paste'),
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide(
@@ -270,16 +457,8 @@ class _GetTestTokensState extends State<GetTestTokens> {
                       height: 56,
                       child: ElevatedButton(
                         onPressed: () {
-                          toast.showToast(
-                            child: CustomToast(widget: widget, isSuccess: true, cancel: () => Fluttertoast.cancel()),
-                            toastDuration: const Duration(seconds: 4),
-                            positionedToastBuilder: (context, child) => Positioned(
-                              top: 30,
-                              left: isTablet ? 70 : 0,
-                              right: 0,
-                              child: child,
-                            ),
-                          );
+                          // _successDialogBuilder(context);
+                          _errorDialogBuilder(context);
                         },
                         style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.all<Color>(
@@ -386,3 +565,7 @@ class CustomToast extends StatelessWidget {
     );
   }
 }
+
+// TODO: Recapcha implementation
+//https://pub.dev/packages/easy_web_view
+//https://developers.google.com/recaptcha/docs/v3
