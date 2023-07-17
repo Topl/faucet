f74dba50eeb30c42e28fd907f3e404d6f646aa11
Enrique Berrios <enrique.noel.berrios@gmail.com>
2023-07-16 22:58:54 -0500

    add selected chain

---
+++ lib/chain/providers/selected_chain_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart';
+import 'package:faucet/chain/models/chains.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final selectedChainProvider = StateProvider<Chains>((ref) {
+  return kDebugMode ? Chains.dev_network : Chains.topl_mainnet;
+});
