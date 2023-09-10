6fd569d1f6976d1b00bae7af7601fd7b8bbf8a57
lukorito <kelvin@stitch.money>
2023-09-10 21:50:17 +0300

    add error message for rate limiting

--- lib/requests/providers/requests_provider.dart
+++ lib/requests/providers/requests_provider.dart
@@ -1,17 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:faucet/requests/models/request.dart';
 import 'package:faucet/requests/providers/rate_limit_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:faucet/requests/utils/get_mock_requests.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
+import 'package:responsive_framework/responsive_row_column.dart';
+
+import '../../shared/theme.dart';
 
 //Future provider used for pagination of requests
 final requestStateAtIndexProvider = FutureProvider.family<Request, int>((ref, index) async {
   return ref.watch(requestProvider.notifier).getRequestFromStateAtIndex(index);
-});
-
-//Future provider used for making requests
-final makeRequestProvider = FutureProvider.family<Request, Request>((ref, request) async {
-  return ref.watch(requestProvider.notifier).makeRequest(request);
 });
 
 final requestProvider = StateNotifierProvider<RequestNotifier, AsyncValue<List<Request>>>((ref) {
@@ -79,28 +79,72 @@ class RequestNotifier extends StateNotifier<AsyncValue<List<Request>>> {
   /// It takes an [index] as a parameter
   ///
   /// It returns a [Future<Request>]
-  Future<Request> makeRequest(Request requestToMake) async {
-    // Check if rate limit is reached
-    final remainingRateLimitTime = ref.read(remainingRateLimitTimeProvider);
-    if (remainingRateLimitTime != null) {
-      throw Exception(
-        'Rate limit reached. Please wait a ${remainingRateLimitTime.inSeconds} seconds and try again.',
-      );
+  Future<Request> makeRequest(BuildContext context, Request requestToMake) async {
+    try {
+      // Check if rate limit is reached
+      final remainingRateLimitTime = ref.read(remainingRateLimitTimeProvider);
+      if (remainingRateLimitTime != null) {
+        throw Exception(
+          'Rate limit reached. Please wait a ${remainingRateLimitTime.inSeconds} seconds and try again.',
+        );
+      }
+
+      final requests = state.asData?.value;
+      if (requests == null) {
+        throw Exception('Error in requestProvider: requests are null');
+      }
+
+      //make request using provided parameters
+      //return Request with transactionId
+      var submittedRequest = requestToMake.copyWith(transactionId: '28EhwUBiHJ3evyGidV1WH8QMfrLF6N8UDze9Yw7jqi6w');
+      requests.add(submittedRequest);
+      state = AsyncData([...requests]);
+
+      ref.read(rateLimitProvider.notifier).setRateLimit();
+
+      return submittedRequest;
+    } catch (e) {
+      _errorDialogBuilder(context, e.toString());
+      throw Exception(e);
     }
-
-    final requests = state.asData?.value;
-    if (requests == null) {
-      throw Exception('Error in requestProvider: requests are null');
-    }
-
-    //make request using provided parameters
-    //return Request with transactionId
-    var submittedRequest = requestToMake.copyWith(transactionId: '28EhwUBiHJ3evyGidV1WH8QMfrLF6N8UDze9Yw7jqi6w');
-    requests.add(submittedRequest);
-    state = AsyncData([...requests]);
-
-    ref.read(rateLimitProvider.notifier).setRateLimit();
-
-    return submittedRequest;
   }
+}
+
+Future<void> _errorDialogBuilder(BuildContext context, String message) {
+  final isMobile = ResponsiveBreakpoints.of(context).equals(MOBILE);
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: ResponsiveRowColumn(
+          layout: isMobile ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+          rowMainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ResponsiveRowColumnItem(
+              child: Text(
+                'Something went wrong...',
+                style: titleLarge(context),
+              ),
+            ),
+            ResponsiveRowColumnItem(
+              child: IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ],
+        ),
+        content: SizedBox(
+          width: 400.0,
+          child: Text(
+            message,
+            style: bodyMedium(context),
+          ),
+        ),
+        actionsPadding: const EdgeInsets.all(16),
+      );
+    },
+  );
 }
