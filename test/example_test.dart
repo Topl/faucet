2335c8eb52c052b9259ee3bff2d3f1e938125e11
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-10-12 15:48:25 -0600

    Tests are freezing

--- test/example_test.dart
+++ test/example_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'essential_test_provider_widget.dart';
+import 'utils/tester_utils.dart';
 
 void main() {
   testWidgets(
@@ -9,9 +10,11 @@ void main() {
       await tester.pumpWidget(
         await essentialTestProviderWidget(),
       );
-
+      print('QQQQ 1');
       await tester.pump();
-      // await pendingTimersFix(tester);
+      print('QQQQ 2');
+      await pendingTimersFix(tester);
+      print('QQQQ 3');
     },
   );
 }
