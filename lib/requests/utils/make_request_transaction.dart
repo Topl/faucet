46e9fe72c63c3146f3e0a694e4342263bd172387
Enrique Berrios <enrique.noel.berrios@gmail.com>
2023-11-13 08:25:58 -0600

    get vaultstore from json

--- lib/requests/utils/make_request_transaction.dart
+++ lib/requests/utils/make_request_transaction.dart
@@ -1,33 +1,34 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:brambldart/brambldart.dart';
-import 'package:servicekit/servicekit.dart';
+import 'dart:io';
 import 'package:topl_common/proto/brambl/models/address.pb.dart';
 import 'package:topl_common/proto/brambl/models/identifier.pb.dart';
 import 'package:topl_common/proto/brambl/models/indices.pb.dart';
 import 'package:topl_common/proto/brambl/models/transaction/io_transaction.pb.dart';
 
 import '../../chain/models/chains.dart';
+import '../utils/json_wallet_key_api.dart';
 import 'native_grpc_channel.dart' if (dart.library.html) 'web_grpc_channel.dart';
 
 Future<IoTransaction> makeRequestTransaction(Chains selectedChain, int amount, String toAddress) async {
   const testnetId = 0x934b1900;
   final transactionBuilderAPI = TransactionBuilderApi(testnetId, 0);
 
-  const walletKeyApi = WalletKeyApi(FlutterSecureStorage());
+  const walletKeyApi = JsonWalletKeyApi();
   final walletApi = WalletApi(walletKeyApi);
-
-  final mainKey = getFaucetMainKey();
-  final childKeys = walletApi.deriveChildKeys(mainKey, Indices(x: 0, y: 0, z: 0));
+  final vaultStore = await walletKeyApi.getMainKeyVaultStore(Platform.environment['NAME']!);
+  final mainKey = walletApi.extractMainKey(vaultStore.right!, Platform.environment['PASSWORD']!.toUtf8());
+  final childKeys = walletApi.deriveChildKeys(mainKey.right!, Indices(x: 0, y: 0, z: 0));
   final signatureTemplate = SignatureTemplate("ExtendedEd25519", 0);
 
   final lockTemplate = PredicateTemplate([signatureTemplate], 1);
   final lock = lockTemplate.build([childKeys.vk]);
   final lockAddress = await transactionBuilderAPI.lockAddress(lock.get());
   final toLockAddress = LockAddress(network: testnetId, ledger: 0, id: LockId(value: toAddress.toUtf8()));
+  //ensure base58 decoding is done properly
+  //uint8 has equals extension
 
   final channel = getChannel(selectedChain);
-
-  LockAddress.
 
   final utxos = await GenusQueryAlgebra(channel).queryUtxo(fromAddress: lockAddress);
   final transaction = await transactionBuilderAPI.buildSimpleLvlTransaction(
