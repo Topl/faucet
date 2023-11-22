f78cbab608aed8057a2efc4539d6f6d0c839ff58
Kelz <makamanekj@gmail.com>
2023-11-22 21:23:19 +0200

    test(DAPP-400): clean up

--- test/requests/tablet_request_failed_test.dart
+++ test/requests/tablet_request_failed_test.dart
@@ -2,9 +2,12 @@ import 'package:faucet/home/sections/get_test_tokens.dart';
 import 'package:faucet/requests/providers/requests_provider.dart';
 import 'package:faucet/shared/services/hive/hive_service.dart';
 import 'package:faucet/transactions/sections/transaction_table.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import '../essential_test_provider_widget.dart';
 import '../required_test_class.dart';
+import '../utils/tester_utils.dart';
 import 'required_request_tests.dart';
 import 'utils/mock_request_hive_utils.dart';
 
@@ -13,58 +16,67 @@ void main() async {
     invalidTestTokenRequest: (testScreenSize) => invalidTestTokenRequest(testScreenSize),
     testScreenSize: TestScreenSizes.tablet,
   );
+
   await requestTests.runTests();
 }
 
 Future<void> invalidTestTokenRequest(TestScreenSizes testScreenSize) async =>
     testWidgets('Tablet - Should fail on invalid test token request', (WidgetTester tester) async {
-      await tester.runAsync(
-        () async {
-          await tester.pumpWidget(
-            await essentialTestProviderWidget(tester: tester, testScreenSize: testScreenSize, overrides: [
-              hivePackageProvider.overrideWithValue(
-                getMockRequestHive().mockHive,
-              ),
-            ]),
-          );
-          await tester.pumpAndSettle();
-          await tester.pumpAndSettle();
-          //   click request token button
-          await tester.ensureVisible(find.byKey(TransactionTableScreen.requestTokensKey));
-          await tester.pumpAndSettle();
-          await tester.pumpAndSettle();
-          Future.delayed(Duration.zero, () {
-            tester.tap(find.byKey(TransactionTableScreen.requestTokensKey));
-          });
-          await tester.tap(find.byKey(TransactionTableScreen.requestTokensKey));
-          Future.delayed(Duration.zero, () {
-            tester.tap(find.byKey(GetTestTokens.requestTokenButtonKey), warnIfMissed: false);
-          });
-          Future.delayed(Duration.zero, () {
-            expect(find.byKey(GetTestTokens.getTestTokensKey), findsOneWidget);
-          });
-          Future.delayed(Duration.zero, () {
-            tester.ensureVisible(find.byKey(GetTestTokens.requestTokenButtonKey));
-          });
-          Future.delayed(Duration.zero, () {
-            tester.tap(find.byKey(GetTestTokens.requestTokenButtonKey));
-          });
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          await essentialTestProviderWidget(
+            tester: tester,
+            testScreenSize: testScreenSize,
+            overrides: [hivePackageProvider.overrideWithValue(getMockRequestHive().mockHive)],
+          ),
+        );
 
-          Future.delayed(Duration.zero, () {
-            expect(find.byKey(SuccessDialog.requestSuccessDialogKey), findsOneWidget);
-          });
-          Future.delayed(Duration.zero, () {
-            tester.tap(find.bySemanticsLabel('Close'), warnIfMissed: false);
-          });
-          Future.delayed(Duration.zero, () {
-            tester.ensureVisible(find.byKey(GetTestTokens.requestTokenButtonKey));
-          });
-          Future.delayed(Duration.zero, () {
-            tester.tap(find.byKey(GetTestTokens.requestTokenButtonKey));
-          });
-          Future.delayed(Duration.zero, () {
-            expect(find.byKey(ErrorDialog.requestErrorDialogKey), findsOneWidget);
-          });
-        },
-      );
+        await tester.pumpAndSettle();
+
+        await tester.ensureVisible(find.byKey(TransactionTableScreen.requestTokensKey));
+
+        await customRunAsync(tester, test: () async {
+          await tester.tap(find.byKey(TransactionTableScreen.requestTokensKey));
+        });
+
+        await tester.pump();
+
+        expect(find.byKey(GetTestTokens.getTestTokensKey), findsWidgets);
+
+        await tester.pump();
+        await tester.ensureVisible(find.byKey(GetTestTokens.requestTokenButtonKey));
+        await tester.pump();
+
+        await customRunAsync(tester, test: () async {
+          await tester.tap(find.byKey(GetTestTokens.requestTokenButtonKey), warnIfMissed: false);
+        });
+        
+        await tester.pumpAndSettle();
+        await tester.pump();
+
+        await customRunAsync(tester, test: () async {
+          expect((find.byKey(SuccessDialog.requestSuccessDialogKey)), findsOneWidget);
+        });
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.byKey(SuccessDialog.closeSuccessDialogKey));
+        await tester.pumpAndSettle();
+
+        expect(find.byKey(GetTestTokens.getTestTokensKey), findsWidgets);
+        await tester.pump();
+
+        await tester.ensureVisible(find.byKey(GetTestTokens.requestTokenButtonKey));
+        await tester.pump();
+
+        await customRunAsync(tester, test: () async {
+          await tester.tap(find.byKey(GetTestTokens.requestTokenButtonKey), warnIfMissed: false);
+        });
+
+        await tester.pumpAndSettle();
+        await tester.pump();
+
+        await customRunAsync(tester, test: () async {
+          expect((find.byKey(ErrorDialog.requestErrorDialogKey)), findsOneWidget);
+        });
+      };
     });
